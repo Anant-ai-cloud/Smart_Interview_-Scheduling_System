0f74f168c9fe5f9419aefdefd7680922dea51213
Anant <anantpathak564@gmail.com>
2026-03-26 20:34:33 +0530

    Get and delete interview completed

--- backend/src/controllers/interviewController.js
+++ backend/src/controllers/interviewController.js
@@ -1,14 +1,14 @@
 import Interview from "../models/interview.model.js";
 
-export const createInterview = async(req, res)=>{
+export const createInterview = async (req, res) => {
     try {
 
         const { candidate, interviewer, date, time, meetingurl, status } = req.body
         const hr = req.user._id
-        if(!candidate || !interviewer || !date  || !time || !meetingurl) return res.status(400).json({message: "All fields are required"})
-        
-       const isInterviewPresent =  await Interview.findOne({candidate: candidate, interviewer: interviewer})
-         if(isInterviewPresent) return res.status(400).json({message: "Already interview is scheduled"})
+        if (!candidate || !interviewer || !date || !time || !meetingurl) return res.status(400).json({ message: "All fields are required" })
+
+        const isInterviewPresent = await Interview.findOne({ candidate: candidate, interviewer: interviewer })
+        if (isInterviewPresent) return res.status(400).json({ message: "Already interview is scheduled" })
 
         const interview = await Interview.create({
 
@@ -19,16 +19,40 @@ export const createInterview = async(req, res)=>{
             time,
             meetingurl,
             status
-            
+
 
         })
-       
+
         return res.status(200).json(interview)
 
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "internal server error" })
+    }
+}
+
+export const getInterviews = async (req, res) => {
+    try {
+        const hrId = req.user._id
+        const interviews = await Interview.find({ hr: hrId }).sort({ createdAt: 1 })
+        return res.status(200).json(interviews)
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "internal server error" })
+    }
+}
+
+export const deleteInterview = async(req, res)=> {
+    try {
+        const id = req.params.id
+        const deleted = await Interview.findByIdAndDelete(id)
+
+        return res.status(200).json({message: "Interview deleted successfully"})
         
     } catch (error) {
         console.log(error)
         return res.status(500).json({message: "internal server error"})
     }
 }
-
--- backend/src/routes/hr.routes.js
+++ backend/src/routes/hr.routes.js
@@ -2,13 +2,15 @@ import express from "express"
 import { gethrUsers } from "../controllers/userController.js"
 import authenticate from "../middlewares/verifyJwt.js"
 import { verifyHr } from "../middlewares/roleChecking.js"
-import { createInterview } from "../controllers/interviewController.js"
+import { createInterview, getInterviews, deleteInterview } from "../controllers/interviewController.js"
 
 const router = express.Router()
 
-router.use(authenticate)
+router.use(authenticate, verifyHr)
 
-router.get("/users", verifyHr, gethrUsers)
-router.post("/interview", verifyHr, createInterview)
+router.get("/users", gethrUsers)
+router.post("/interview", createInterview)
+router.get("/get/interviews", getInterviews)
+router.delete("/remove/:id", deleteInterview)
 
 export default router
