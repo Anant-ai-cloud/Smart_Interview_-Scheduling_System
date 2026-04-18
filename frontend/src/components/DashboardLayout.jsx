691a478334bb1f315e4f17b195e808aac282d0c0
Anant <anantpathak564@gmail.com>
2026-04-19 00:13:08 +0530

    Setting up Link and fixing ui

--- frontend/src/components/DashboardLayout.jsx
+++ frontend/src/components/DashboardLayout.jsx
@@ -5,11 +5,15 @@ import { Outlet } from 'react-router-dom'
 
 function DashboardLayout() {
   return (
-    <div>
+    <div className='relative'>
       
+  
       <Sidebar/>
       <Topbar/>
+      
+      <div className='absolute left-96 top-20'>
       <Outlet/>
+      </div>
       
     </div>
   )
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,51 +1,5 @@
-// import React from "react";
+import { Link } from "react-router-dom";
 
-// const Sidebar = () => {
-//   return (
-//     <aside className="fixed left-0 top-0 h-screen w-64 bg-[#fff9ec] z-50 flex flex-col">
-//       <div className="p-8">
-//         <div className="flex items-center gap-3">
-//           <div className="w-10 h-10 rounded-xl bg-orange-500 flex items-center justify-center text-white">
-//             ✨
-//           </div>
-//           <div>
-//             <h1 className="text-xl font-black">Alchemist Admin</h1>
-//             <p className="text-[10px] uppercase">Interview Intelligence</p>
-//           </div>
-//         </div>
-//       </div>
-
-//       <nav className="flex-1 px-4 space-y-2">
-//         {["Dashboard", "User Management", "Schedules", "Feedback"].map(
-//           (item, i) => (
-//             <div
-//               key={i}
-//               className={`px-4 py-3 rounded-xl cursor-pointer ${
-//                 item === "Schedules"
-//                   ? "bg-orange-200 font-bold"
-//                   : "hover:bg-orange-100"
-//               }`}
-//             >
-//               {item}
-//             </div>
-//           )
-//         )}
-//       </nav>
-
-//       <div className="p-8">
-//         <div className="bg-yellow-100 rounded-xl p-4">
-//           <p className="text-xs font-bold">QUICK STATS</p>
-//           <div className="flex justify-between">
-//             <span className="text-2xl font-black">12</span>
-//             <span className="text-xs">TODAY</span>
-//           </div>
-//         </div>
-//       </div>
-//     </aside>
-//   );
-// };
-
-// export default Sidebar;
 function Sidebar() {
   return (
     <aside className="fixed left-0 top-0 h-full w-64 bg-[#fff9ec] z-50">
@@ -54,9 +8,9 @@ function Sidebar() {
       </div>
 
       <nav className="px-4 space-y-2">
-        <a className="flex gap-3 p-3 bg-amber-100 rounded-lg">Dashboard</a>
+        <Link to={"/admindashboard"} className="flex gap-3 p-3 hover:bg-amber-100 rounded-lg">Dashboard</Link>
         <a className="flex gap-3 p-3 hover:bg-amber-100">Users</a>
-        <a className="flex gap-3 p-3 hover:bg-amber-100">Schedules</a>
+        <Link to={"/schedule"} className="flex gap-3 p-3 hover:bg-amber-100">Schedules</Link>
         <a className="flex gap-3 p-3 hover:bg-amber-100">Feedback</a>
       </nav>
     </aside>
